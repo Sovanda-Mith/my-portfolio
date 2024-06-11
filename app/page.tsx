5d4b9b11d73c419102f73b1ddc793850c201d391
MITH Sovanda <vanda.can.read@gmail.com>
2024-06-11 16:33:48 +0700

    change background

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="h-dvh bg-neutral-900">
-        <Nav />
-        <div className="flex flex-col justify-center items-center mt-40 text-white">
-          <Image
-            className="border-2 border-black rounded-lg"
-            src="/my-pf.png"
-            width={200}
-            height={200}
-            alt="Picture of the author"
-          ></Image>
-          <div className="mt-7 text-4xl hover:text-cyan-500">
-            Student, Developer & Game Enthusiast
+      <div className="h-dvh w-full bg-neutral-900 relative">
+        <video
+          src="/video_background.mp4"
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover"
+        ></video>
+        <div className="absolute top-0 w-full h-full">
+          <Nav />
+          <div className="flex flex-col justify-center items-center mt-40 text-white">
+            <Image
+              className="border-2 border-black rounded-lg"
+              src="/my-pf.png"
+              width={200}
+              height={200}
+              alt="Picture of the author"
+            ></Image>
+            <div className="mt-7 text-4xl hover:text-cyan-500">
+              Student, Developer & Game Enthusiast
+            </div>
+            <Social />
           </div>
-          <Social />
         </div>
       </div>
       <div className="bg-white text-black p-10">
